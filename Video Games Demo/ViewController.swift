268c2215c50756a0a0bd6b83a828ec6a0044de69
Mohammad Shaker <mohamed.shaker@itworx.com>
2019-07-19 21:14:22 +0200

    Initial Commit

---
+++ Video Games Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video Games Demo
+//
+//  Created by Mohammad Shaker on 7/19/19.
+//  Copyright © 2019 Mohammad Shaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
